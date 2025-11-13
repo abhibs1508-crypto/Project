1896d6a539167fbe2c7e197775730bf1d95d1427
abhibs1508-crypto <abhibs1508@gmail.com>
2025-11-13 13:02:55 +0530

    a1201

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,34 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Import your components/pages
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
-import ServiceDetail from "./pages/ServiceDetail";
 import Contact from "./pages/Contact";
+import Gallery from "./pages/Gallery";
+// import GalleryView from "./pages/GalleryView";
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Gallery from "./components/Gallery";
 
-function App() {
+export default function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/services/:id" element={<ServiceDetail />} /> {/* ✅ Detail Page */}
-        <Route path="/gallery" element={<Gallery />} />
-         <Route path="/contact" element={<Contact />} />
-      </Routes>
-      {/* <Footer /> */}
+      <div className="app">
+        <Navbar />
+
+        {/* Main Page Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/gallery" element={<Gallery />} />
+          {/* <Route path="/gallery/:category" element={<GalleryView />} /> */}
+        </Routes>
+
+      </div>
     </Router>
   );
 }
-
-export default App;
